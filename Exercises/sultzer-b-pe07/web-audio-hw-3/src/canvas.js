e61f30dd2d89e68d1ba92eece9b0318b3597774c
BenSultzer <bms3902@rit.edu>
2024-02-26 23:57:45 -0500

    Fixed noise color.

--- Exercises/sultzer-b-pe07/web-audio-hw-3/src/canvas.js
+++ Exercises/sultzer-b-pe07/web-audio-hw-3/src/canvas.js
@@ -136,7 +136,7 @@ function draw(params = {}) {
             // data[i+2] is the blue channel
             // data[i+3] is the alpha channel
             data[i] = data[i + 1] = data[i + 2] = 0; // zero out the red and green and blue channels
-            data[i] = 255; // make the red channel 100% red
+            data[i] = 10; // Grey noise
         } // end if
 
         // invert?
