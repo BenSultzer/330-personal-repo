dcfa4f258e3168a903e68189e0198038003f849d
BenSultzer <bms3902@rit.edu>
2024-04-07 11:32:35 -0400

    Removed all transpiling and bundling code from HW-3 work, just have to go back through all files and make tiny tweaks to get it back to original HW-2 form. Should still work afterwards and can continue on with HW-3 instructions.

---
+++ HomeworkAssignments/sultzer-b-hw2-raw/src/classes/ParticleSystem.js
@@ -0,0 +1,55 @@
+// Import the Particle class
+import { Particle } from "./Particle.js";
+
+// Import helper functions
+import * as utils from '../utils.js';
+
+// Overview: Homework 2
+// Author: Ben Sultzer <bms3902@rit.edu>
+// Description: Class that creates an explosion at a point
+class ParticleSystem {
+    // Initializes the data necessary for a particle system
+    // "positionX" parameter: The x-coordinate of the particle system's origin
+    // "positionY" parameter: The y-coordinate of the particle system's origin
+    // "numParticles" parameter: The number of particles in this particle system
+    // Returns: Nothing
+    constructor(positionX, positionY, numParticles, particleRadius, particleColor) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.particles = new Array(numParticles);
+        this.particleRadius = particleRadius;
+        this.particleColor = particleColor;
+
+        // Initialize the particle system
+        this.createParticleSystem();
+    }
+
+    // Creates the particle system with the indicated number of particles
+    // Returns: Nothing
+    createParticleSystem() {
+        // Loop through this particle system's list of particles and initialize all particles
+        for(let i = 0; i < this.particles.length; i++) {
+            this.particles[i] = new Particle(this.positionX, 
+                                             this.positionY, 
+                                             this.particleRadius, 
+                                             this.particleColor, 
+                                             utils.getRandom(100, 200), 
+                                             new Array(utils.getRandom(-1, 1), utils.getRandom(-1, 1)));
+        }
+    }
+
+    // Udpates all particles in this particle system to create an explosion!
+    // "ctx" parameter: The 2D drawing context to use for drawing
+    // "deltaTime" parameter: The time that has passed since last frame
+    // Returns: Nothing
+    update(ctx, deltaTime) {
+        // Loop through all particles, updating their data then drawing them
+        for (let i = 0; i < this.particles.length; i++) {
+            this.particles[i].update(deltaTime);
+            this.particles[i].draw(ctx);
+        }
+    }
+}
+
+// Export the ParticleSystem class
+export { ParticleSystem };
---
+++ HomeworkAssignments/sultzer-b-hw2-raw/src/loader.js
@@ -0,0 +1,16 @@
+// Overview: Homework 2
+// Author: Ben Sultzer <bms3902@rit.edu>
+
+// Get main.js's init() function
+import * as main from "./main.js";
+
+// Starts up the app
+// Parameters: None
+// Returns: Nothing
+window.onload = () => {
+    console.log("window.onload called");
+    // 1 - do preload here - load fonts, images, additional sounds, etc...
+
+    // 2 - start up app
+    main.init();
+}
