3a0b900e48ce27538eda9197e38eee49bac69762
BenSultzer <bms3902@rit.edu>
2024-04-21 16:18:45 -0400

    About to start Section IV of HW-4 Starter

--- Exercises/sultzer-b-hw4-starter/src/main.js
+++ Exercises/sultzer-b-hw4-starter/src/main.js
@@ -1,3 +1,7 @@
+// Overview: HW-4 - NYS Park Buddy - Starter
+// Author: Ben Sultzer <bms3902@rit.edu>
+
+// Import the map and JSON loading functionality
 import * as map from "./map.js";
 import * as ajax from "./ajax.js";
 
@@ -8,23 +12,39 @@ const lnglatUSA = [-98.5696, 39.8282];
 
 
 // II. Functions
+// Hooks up the various UI elements of the NY State Park Buddy app
+// Parameters: None
+// Returns: Nothing
 const setupUI = () => {
-	// NYS Zoom 5.2
+	// Button for setting map to NYS at Zoom 5.2
 	document.querySelector("#btn-1").onclick = () => {
 		map.setZoomLevel(5.2);
 		map.setPitchAndBearing(0, 0);
 		map.flyTo(lnglatNYS);
 	};
 	
-	// NYS isometric view
+	// Button for setting map to NYS in an isometric view
+	document.querySelector("#btn-2").onclick = () => {
+		map.setZoomLevel(5.5);
+		map.setPitchAndBearing(45, 0);
+		map.flyTo(lnglatNYS);
+	};
 	
-	// World zoom 0
-
+	// Button for setting map to the USA at Zoom 3
+	document.querySelector("#btn-3").onclick = () => {
+		map.setZoomLevel(3);
+		map.setPitchAndBearing(0, 0);
+		map.flyTo(lnglatUSA);
+	};
 }
 
+// Sets up the map and prepares the UI
+// Parameters: None
+// Returns: Nothing
 const init = () => {
 	map.initMap(lnglatNYS);
 	setupUI();
 };
 
+// Start up the app
 init();
--- Exercises/sultzer-b-hw4-starter/src/map.js
+++ Exercises/sultzer-b-hw4-starter/src/map.js
@@ -1,5 +1,10 @@
+// Overview: HW-4 - NYS Park Buddy - Starter
+// Author: Ben Sultzer <bms3902@rit.edu>
+
 // I. Variables & constants
+// Key needed to access Mapbox
 const accessToken = "pk.eyJ1IjoiY2F0b2dyYXBoZXIiLCJhIjoiY2x1ZzFucGE5MjdkdTJrbWZqaWI1ODNveSJ9.U2o3E6ioE1zRpGEmwF5-9Q";
+// Map object
 let map;
 
 // An example of how our GeoJSON is formatted
@@ -8,7 +13,7 @@ let geojson = {
 	type: "FeatureCollection",
 	features: [{
 		"type": "Feature",
-		"id" : "p79",
+		"id": "p79",
 		"properties": {
 			"title": "Letchworth State Park",
 			"description": "Letchworth State Park, renowned as the \"Grand Canyon of the East,\".",
@@ -27,33 +32,74 @@ let geojson = {
 };
 
 // II. "private" - will not be exported
+// Sets up the Mapbox instance
+// "center" parameter: Location to focus on when the map is created
+// Returns: Nothing
 const initMap = (center) => {
+	// Get the access token
 	mapboxgl.accessToken = accessToken;
 
+	// Create the map
 	map = new mapboxgl.Map({
 		container: "map",
 		style: "mapbox://styles/mapbox/light-v11",
 		center: center,
 		zoom: 5.2
 	});
-	map.addControl(new mapboxgl.NavigationControl({showCompass:false}));
+
+	// Add a zoom-in and zoom-out control (Plus/minus sign buttons)
+	map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
+};
+
+// Adds a marker to the map on a "Point of Interest" (POI)
+// "feature" parameter: Contains info about the POI
+// "className" parameter: The HTML class name for POIs (for CSS formatting)
+// "clickHandler" parameter: Function to call if the marker is clicked
+// Returns: Nothing
+const addMarker = (feature, className, clickHandler) => {
+	// A. Create a map marker using feature (i.e. "Park") data
+	// - the marker is a <div>
+	// - <div> className will be "poi" - see default-styles.css to see the details
+	// - note that we give the <div> the id of the "feature"
+	const el = document.createElement("div");
+	el.className = className;
+	el.id = feature.id;
+
+	// B. This the HTML for the Popup
+	const html = `
+		<b>${feature.properties.title}</b>
+		<p>${feature.properties.address}</p>
+		<p><b>Phone: </b>${feature.properties.phone}</p>
+	`;
+
+	// C. Make the marker, add a popup, and add to map
+	// https://docs.mapbox.com/mapbox-gl-js/api/markers/#marker
+	// https://docs.mapbox.com/mapbox-gl-js/api/markers/#popup
+	const marker = new mapboxgl.Marker(el)
+		.setLngLat(feature.geometry.coordinates)
+		.setPopup(new mapboxgl.Popup({ offset: 10 })
+			.setHTML(html))
+		.addTo(map);
+
+	// D. Call this method when marker is clicked on
+	el.addEventListener("click", () => clickHandler(marker._element.id));
 };
 
 
 // III. "public" - will be exported
 
 
-const flyTo = (center = [0,0]) => {
+const flyTo = (center = [0, 0]) => {
 	//https://docs.mapbox.com/mapbox-gl-js/api/#map#flyto
 	map.flyTo({ center: center });
 };
 
-const setZoomLevel = (value=0) => {
+const setZoomLevel = (value = 0) => {
 	// https://docs.mapbox.com/help/glossary/zoom-level/
 	map.setZoom(value);
 };
 
-const setPitchAndBearing = (pitch=0,bearing=0) => {
+const setPitchAndBearing = (pitch = 0, bearing = 0) => {
 	// https://docs.mapbox.com/mapbox-gl-js/example/live-update-feature/
 	// https://docs.mapbox.com/mapbox-gl-js/example/set-perspective/
 	map.setPitch(pitch);
